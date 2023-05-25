a9029b00f791bc5ffc3cd461255ec437ca29caf3
Bright <108338051+I3right@users.noreply.github.com>
2023-05-25 22:31:54 +0700

    css skill icon

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -18,19 +18,19 @@ const Skills = () => {
         <h2 id="skills" className="section-name h2-margin">skills</h2>
         <div className="skills col-sm-12 col-lg-12">
         <div className='row align-items-center justify-content-center' id="skills_row1">
-          <figure className=" col d-flex justify-content-center"><img src={Html} className="skillsPicture " alt="Html logo" /></figure>
-          <figure className=" col d-flex justify-content-center"><img src={Css} className="skillsPicture " alt="Css logo" /></figure>
-          <figure className=" col d-flex justify-content-center"><img src={Bootstrap} className="skillsPicture " alt="Bootstrap logo" /></figure>
-          <figure className=" col d-flex justify-content-center"><img src={Javascript} className="skillsPicture " alt="Javascript logo" /></figure>
-          <figure className=" col d-flex justify-content-center"><img src={Reacts} className="skillsPicture " alt="React logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Html} className="skillsPicture skill1 " alt="Html logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Css} className="skillsPicture skill1" alt="Css logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Bootstrap} className="skillsPicture skill1" alt="Bootstrap logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Javascript} className="skillsPicture skill1" alt="Javascript logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Reacts} className="skillsPicture skill1" alt="React logo" /></figure>
         </div>
 
         <div className='row align-items-center justify-content-center'  id="skills_row2">
-          <figure className=" col d-flex justify-content-center"><img src={Nodejs} className="skillsPicture " alt="Nodejs logo" /></figure>
-          <figure className=" col d-flex justify-content-center"><img src={VScode} className="skillsPicture " alt="VScode logo" /></figure>
-          <figure className=" col d-flex justify-content-center"><img src={Git} className="skillsPicture " alt="Git logo" /></figure>
-          <figure className=" col d-flex justify-content-center"><img src={Github} className="skillsPicture " alt="Github logo" /></figure>
-          <figure className=" col d-flex justify-content-center"><img src={Figma} className="skillsPicture " alt="Figma logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Nodejs} className="skillsPicture skill2" alt="Nodejs logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={VScode} className="skillsPicture skill2" alt="VScode logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Git} className="skillsPicture skill2" alt="Git logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Github} className="skillsPicture skill2" alt="Github logo" /></figure>
+          <figure className=" col d-flex justify-content-center"><img src={Figma} className="skillsPicture skill2" alt="Figma logo" /></figure>
         </div>
         </div>
     </section>
