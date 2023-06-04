871863e60a550f7ffc6c1d433f4a61826a10f8ef
Bright <108338051+I3right@users.noreply.github.com>
2023-06-04 13:34:31 +0700

    wording static page

--- src/component/project/Colmar.jsx
+++ src/component/project/Colmar.jsx
@@ -9,7 +9,7 @@ const Colmar = () => {
       </figure>
       <figcaption>
         <div className="card-head d-flex justify-content-between align-items-center ">
-          <h3><strong>landingpage</strong></h3>
+          <h3><strong>Static page</strong></h3>
           <p><small>JSD4</small></p>
         </div>
         <div className="card-sub-head">
--- src/component/project/Twitter.jsx
+++ src/component/project/Twitter.jsx
@@ -9,7 +9,7 @@ const Twitter = () => {
       </figure>
       <figcaption>
         <div className="card-head d-flex justify-content-between align-items-center ">
-          <h3><strong>landingpage</strong></h3>
+          <h3><strong>Static page</strong></h3>
           <p><small>self</small></p>
         </div>
         <div className="card-sub-head">
